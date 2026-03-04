c029fb8ac1bb7563297d3efc0f020c757c992c49
Asamoah Bamfo Israel Kenneth <israel.kenneth2011@gmail.com>
2026-03-04 10:39:12 +0100

    continuity

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div className={"text-amber-500 hover:text-amber-950 "}>Home</div>
+        <section>
+            <h1 className="text-center"> The Hub for Every Dev Event You Can&#39;t Miss </h1>
+            <p className="text-center mt-10">Hackathons, Meetups, and Conferences, all in one place</p>
+        </section>
     )
 }
 export default Home
